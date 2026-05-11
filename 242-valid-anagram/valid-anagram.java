9816258e8b8c81d4cad598f79be52a7a489ca03e
Piyush Kumar Kharga <piyushkumar0617@gmail.com>
2026-05-11 23:24:07 +0530

    Time: 4 ms (82.12%) | Memory: 46.5 MB (23.62%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] charArray1 = s.toCharArray();
+        Arrays.sort(charArray1);
+        char[] charArray2 = t.toCharArray();
+        Arrays.sort(charArray2);
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
