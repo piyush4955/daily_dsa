b053de4da9b3297cab90468e9b4ae64f02caa6ac
Piyush Kumar Kharga <piyushkumar0617@gmail.com>
2026-05-24 09:06:35 +0530

    Time: 244 ms (5.30%) | Memory: 48.6 MB (5.09%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,8 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String clean = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        String ans = new StringBuilder(clean).reverse().toString();
-        if(clean.equals(ans)) return true;
-        else return false;
+        String og = s.toLowerCase();
+        String ogtemp = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((og.charAt(i)>='a' && og.charAt(i)<='z') || (og.charAt(i)>='0' && og.charAt(i)<='9')) {
+                ogtemp += og.charAt(i);
+            } else {
+                continue;
+            }
+        }
+
+        String ogrev = "";
+        int j = 0;
+
+        for (int i = ogtemp.length() - 1; i >= 0; i--) {
+            ogrev += ogtemp.charAt(i);
+            j++;
+        }
+
+        for (int i = 0; i < ogtemp.length(); i++) {
+            if (ogrev.charAt(i) != ogtemp.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
