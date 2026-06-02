c8dde69f5b6deb826117258d85d65570eca41035
Piyush Kumar Kharga <piyushkumar0617@gmail.com>
2026-06-02 08:53:36 +0530

    Time: 9 ms (98.55%) | Memory: 47.1 MB (49.52%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
@@ -3,21 +3,20 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int diff = 0;
-        if(n == 1){
-            return 0;
-        }
-        else if(n==2){
-            return Math.abs(nums[1]-nums[0]);
-        }
+        // if(n == 1){
+        //     return 0;
+        // }
+        // else if(n==2){
+        //     return Math.abs(nums[1]-nums[0]);
+        // }
         
-        else {
-            int mid = n/2;
-            int median = nums[mid];
-            for(int i = 0; i<n; i++){
-                diff += Math.abs(nums[i]-median);
-            }
-            return diff;
+        int mid = n/2;
+        int median = nums[mid];
+        for(int i = 0; i<n; i++){
+            diff += Math.abs(nums[i]-median);
         }
+        return diff;
+        
         // else{
         //     int mid1 = n/2;
         //     int mid2 = n/2+1;
