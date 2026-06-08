498ebf6fbea0fa5ab3737a1cd3dd980bf0a62d1e
Piyush Kumar Kharga <piyushkumar0617@gmail.com>
2026-06-08 09:05:09 +0530

    Time: 5 ms (73.56%) | Memory: 159.9 MB (49.83%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,34 +1,32 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> less = new ArrayList<>();
-        ArrayList<Integer> equal = new ArrayList<>();
-        ArrayList<Integer> greater = new ArrayList<>();
-        int ans[] = new int[nums.length];
+        int less = 0;
+        int equal = 0;
 
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]<pivot){
-                less.add(nums[i]);
-            }
-            else if(nums[i]==pivot){
-                equal.add(pivot);
-            }
-            else{
-                greater.add(nums[i]);
+        for (int num : nums) {
+            if (num < pivot) {
+                less++;
+            } else if (num == pivot) {
+                equal++;
             }
         }
-        for(int i = 0; i<less.size(); i++){
-            ans[i] = less.get(i);
+
+        int[] ans = new int[nums.length];
+
+        int i = 0;              // start of < pivot region
+        int j = less;           // start of == pivot region
+        int k = less + equal;   // start of > pivot region
+
+        for (int num : nums) {
+            if (num < pivot) {
+                ans[i++] = num;
+            } else if (num == pivot) {
+                ans[j++] = num;
+            } else {
+                ans[k++] = num;
+            }
         }
-        int j = 0;
-        for(int i = less.size(); i<less.size()+equal.size(); i++){
-            ans[i] = equal.get(j);
-            j++;
-        }
-        int k = 0;
-        for(int i = less.size()+equal.size(); i<nums.length; i++){
-            ans[i] = greater.get(k);
-            k++;
-        }
+
         return ans;
     }
 }
