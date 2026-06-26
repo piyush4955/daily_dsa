6e6cdc8042e5ee215abbc2af068647b71737e311
Piyush Kumar Kharga <piyushkumar0617@gmail.com>
2026-06-26 19:27:24 +0530

    Time: 3 ms (9.42%) | Memory: 46.5 MB (25.89%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        int i = 0;
+        int j = 0;
+        int n = pushed.length;
+
+        while(i<n){
+            stack.push(pushed[i]);
+            while(!stack.isEmpty() && j<n && stack.peek()==popped[j]){
+                stack.pop();
+                j++;
+            }
+            i++;
+        }
+        if(stack.size()==0){
+            return true;
+        }
+        return false;
+    }
+}
